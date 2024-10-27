e61a38c7e51f77e32590393083452fc44c6adfa4
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2024-10-27 23:11:09 +0530

    day 1 login and logout

---
+++ rps_arena/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('leader_boards/', views.leader_boards, name='leader_boards'),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('profile/', views.profile, name='profile'),
+]
