01a9368f273a790b2be676006d4ab8d1e400cae4
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2025-02-03 20:52:41 +0530

    moved os.environ.setdefault above get_asgi

--- battle_hands_project/asgi.py
+++ battle_hands_project/asgi.py
@@ -14,10 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battle_hands_project.settings")
 django_asgi_app = get_asgi_application()
 
 from rps_arena.routing import websocket_urlpatterns
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battle_hands_project.settings")
 
 application = ProtocolTypeRouter(
     {
