7a538c8fa2bdd939dd516d4e0cc36c685dbe7752
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2025-02-03 22:28:03 +0530

    more db settings change

--- battle_hands_project/settings.py
+++ battle_hands_project/settings.py
@@ -90,15 +90,15 @@ WSGI_APPLICATION = 'battle_hands_project.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('DB_NAME'),
-    #     'USER': os.getenv('DB_USER'),
-    #     'PASSWORD': os.getenv('DB_PASSWORD'),
-    #     'HOST': os.getenv('DB_HOST'),  # Set a proper default
-    #     'PORT': os.getenv('DB_PORT'), # 3306-mysql , 5432-postgresql
-    # }
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'), # 3306-mysql , 5432-postgresql
+    }
+    # 'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
