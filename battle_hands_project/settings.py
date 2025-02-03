ce3c7371d042686d896be53d0b1d169d36d065a7
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2025-02-03 21:02:26 +0530

    change allowed_host

--- battle_hands_project/settings.py
+++ battle_hands_project/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'battle-hands.onrender.com',
+    '127.0.0.1',
+    'localhost',
+    ]
 
 
 # Application definition
